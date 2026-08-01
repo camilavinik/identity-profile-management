0d56f88ebbdb0b000dc35ca36ba9e3625c9ecb3c
camilavinik <contactcamilasanchez@gmail.com>
2026-08-01 15:43:40 -0300

    prepare backend deploy

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,12 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: process.env.FRONTEND_URL?.split(',') ?? ['http://localhost:5173'],
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
